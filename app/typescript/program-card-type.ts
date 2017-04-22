cf4f1aa84335b89cdba8336d014cdbacdf4a02e3
Mike Darwish <michael@darwish.ca>
2017-04-22 14:47:46 -0400

    Stuff

---
+++ app/typescript/program-card-type.ts
@@ -0,0 +1,4 @@
+﻿enum ProgramCardType {
+    MOVE,
+    ROTATE
+}
--- app/typescript/robot-phase-action.ts
+++ app/typescript/robot-phase-action.ts
@@ -1,3 +1,3 @@
-﻿class RobotPhase {
+﻿class RobotPhaseAction {
     constructor(public robot: Robot, public programCard: ProgramCard) { }
 }
