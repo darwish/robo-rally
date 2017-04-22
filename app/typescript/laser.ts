9a551cd74615b71b7bd4498547b57c926749cc65
Stephane Beniak <stephane.beniak@gmail.com>
2017-04-22 14:46:25 -0400

    Lasers should now work.

--- app/typescript/laser.ts
+++ app/typescript/laser.ts
@@ -11,12 +11,34 @@
 
             if (this.facingDirection == Direction.E
                 && robots[i].position.y == this.position.y
-                && robots[i].position.x <= this.position.x) {
+                && robots[i].position.x >= this.position.x
+                && (closestFacingRobot == null || robots[i].position.x < closestFacingRobot.x)) {
+
+                closestFacingRobot = robots[i];
+            }
+            else if (this.facingDirection == Direction.W
+                && robots[i].position.y == this.position.y
+                && robots[i].position.x <= this.position.x
+                && (closestFacingRobot == null || robots[i].position.x > closestFacingRobot.x)) {
+
+                closestFacingRobot = robots[i];
+            }
+            else if (this.facingDirection == Direction.N
+                && robots[i].position.x == this.position.x
+                && robots[i].position.y >= this.position.y
+                && (closestFacingRobot == null || robots[i].position.y < closestFacingRobot.y)) {
+
+                closestFacingRobot = robots[i];
+            }
+            else if (this.facingDirection == Direction.S
+                && robots[i].position.x == this.position.x
+                && robots[i].position.y <= this.position.y
+                && (closestFacingRobot == null || robots[i].position.y > closestFacingRobot.y)) {
 
                 closestFacingRobot = robots[i];
             }
         }
 
-        closestFacingRobot.dealDamage(damagePower);
+        closestFacingRobot.dealDamage(this.damagePower);
     }
 }
