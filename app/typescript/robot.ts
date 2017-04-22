6f0509ea36496e57ed1ea0d9679e21df51922b60
Dan Greencorn <dan@dangreencorn.ca>
2017-04-22 14:01:49 -0400

    Updates

--- app/typescript/robot.ts
+++ app/typescript/robot.ts
@@ -1,1 +1,18 @@
-﻿class Robot {}
+class Robot {
+
+    public isPoweredDown: boolean;
+    public optionCards: Array<OptionCard>;
+
+    constructor(public position: BoardPosition, public orientation: Direction, public lives: number, public health?: number) {
+        if (health == undefined) {
+            this.health = 9;
+        }
+
+        this.isPoweredDown = false;
+        this.optionCards = [];
+    }
+
+    public isDead() {
+        return this.health == 0;
+    }
+}
